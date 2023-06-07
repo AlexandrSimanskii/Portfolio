18f10e0137dfb6d1a5f2b3186ec2722537cff388
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2023-06-07 10:10:04 +0200

    утро 7

--- script/script.js
+++ script/script.js
@@ -1,17 +1,28 @@
 // scroll seection!!!
+
+// window.onscroll = () => {
+//   let header = document.querySelector(".header");
+//   header.classList.toggle("header-sticky", window.scrollY > 100);
+// };
+
 let section = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll(".header .navbar a");
 
 window.onscroll = () => {
+  let header = document.querySelector(".header");
+  header.classList.toggle("header-sticky", window.scrollY > 100);
+  
   section.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 100;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('')
+    let id = sec.getAttribute("id");
+console.log(offset);
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        // document.querySelector(".navbar a ").classList.add('active');
+      });
+    }
   });
-};
-
-window.onscroll = () => {
-  let header = document.querySelector(".header");
-  header.classList.toggle("header-sticky", window.scrollY > 100);
 };
