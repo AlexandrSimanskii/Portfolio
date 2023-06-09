2fc9785a43d0b7684d78a674601fe8b12149ef64
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2023-06-09 23:38:04 +0200

    9 evening

--- script/script.js
+++ script/script.js
@@ -1,28 +1,51 @@
+// Работа с navbar в уменьшеном header
+let menuIcon = document.querySelector("#menu-icons img");
+let navBar = document.querySelector(".navbar");
+let navbarSpan = document.querySelector(".navbar span");
+
+menuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  navbarSpan.classList.toggle("active-nav");
+  if (menuIcon.getAttribute("src") === "/src/images/icons/icons8-menu-50.png") {
+    menuIcon.src = "/src/images/icons/icons8-close-50.png";
+  } else {
+    menuIcon.src = "/src/images/icons/icons8-menu-50.png";
+  }
+});
+
 // scroll seection!!!
 
-// window.onscroll = () => {
-//   let header = document.querySelector(".header");
-//   header.classList.toggle("header-sticky", window.scrollY > 100);
-// };
-
 let section = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll(".header .navbar a");
+let navLinks = document.querySelectorAll(".header nav a");
 
 window.onscroll = () => {
   let header = document.querySelector(".header");
   header.classList.toggle("header-sticky", window.scrollY > 100);
-  
+
   section.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 100;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
-console.log(offset);
+
+    // Даю .activ нужным link в navbar
     if (top >= offset && top < offset + height) {
       navLinks.forEach((links) => {
         links.classList.remove("active");
-        // document.querySelector(".navbar a ").classList.add('active');
+        document
+          .querySelector(`.header nav a[href*=${id}]`)
+          .classList.add("active");
       });
-    }
+
+      // add style:show-animation for section on skroll
+      sec.classList.add("show-animate");
+    }else{sec.classList.remove("show-animate");}
   });
 };
+
+navLinks.forEach((a) => {
+  a.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    menuIcon.src = "/src/images/icons/icons8-menu-50.png";
+  });
+});
