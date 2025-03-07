c26d899e4caa5926f6caefc4c614eb42460cd41e
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2025-03-07 18:05:32 +0200

    add my text

--- script/script.js
+++ script/script.js
@@ -61,21 +61,22 @@ phoneInput.addEventListener("input", function (e) {
   let value = e.target.value.replace(/\D/g, "");
   if (value.length > 11) value = value.substring(0, 11);
 
-  let formattedValue = `+7 (${e.data}`;
+  let formattedValue = `+7 `;
 
-  if (value.length > 1) {
-    formattedValue += `${value.substring(1, 4)}`;
-  }
-  // if (value.length >= 4) formattedValue += `) ${value.substring(4, 7)}`;
-  // if (value.length >= 7) formattedValue += `-${value.substring(7, 9)}`;
-  // if (value.length >= 9) formattedValue += `-${value.substring(9, 11)}`;
+  if (value.length == 1) formattedValue += `(${e.data}`;
+  if (value.length > 1) formattedValue += `(${value.substring(1, 4)}`;
+  if (value.length >= 4) formattedValue += `) ${value.substring(4, 7)}`;
+  if (value.length >= 7) formattedValue += `-${value.substring(7, 9)}`;
+  if (value.length >= 9) formattedValue += `-${value.substring(9, 11)}`;
 
   e.target.value = formattedValue;
 });
 
 phoneInput.addEventListener("keydown", function (e) {
   if (e.key === "Backspace") {
-    e.target.value = "";
+    let value = e.target.value.slice(0, -1);
+    e.target.value=value
+
     e.preventDefault();
   }
 });
