24b07862923ff22a800a6db83aaa4eac4170656c
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2025-03-07 16:42:43 +0200

    start create inputMask

--- script/script.js
+++ script/script.js
@@ -54,3 +54,28 @@ navLinks.forEach((a) => {
 // let test = document.querySelectorAll(".about.show-animate .animate.scroll");
 
 // Animation footer on scroll !!!
+
+const phoneInput = document.getElementById("phone");
+
+phoneInput.addEventListener("input", function (e) {
+  let value = e.target.value.replace(/\D/g, "");
+  if (value.length > 11) value = value.substring(0, 11);
+
+  let formattedValue = `+7 (${e.data}`;
+
+  if (value.length > 1) {
+    formattedValue += `${value.substring(1, 4)}`;
+  }
+  // if (value.length >= 4) formattedValue += `) ${value.substring(4, 7)}`;
+  // if (value.length >= 7) formattedValue += `-${value.substring(7, 9)}`;
+  // if (value.length >= 9) formattedValue += `-${value.substring(9, 11)}`;
+
+  e.target.value = formattedValue;
+});
+
+phoneInput.addEventListener("keydown", function (e) {
+  if (e.key === "Backspace") {
+    e.target.value = "";
+    e.preventDefault();
+  }
+});
